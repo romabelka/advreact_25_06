9dd573f6ed81ea5a7bb90774c7be5773a76c3610
Roman Iakobchuk <romania@wix.com>
2018-07-05 20:33:31 +0300

    add simple test

--- admin/src/ducks/people.js
+++ admin/src/ducks/people.js
@@ -2,6 +2,8 @@ import { appName } from '../config'
 import { Record, List } from 'immutable'
 import { reset } from 'redux-form'
 import { createSelector } from 'reselect'
+import { put, takeEvery, call } from 'redux-saga/effects'
+import { generateId } from './utils'
 
 /**
  * Constants
@@ -9,6 +11,7 @@ import { createSelector } from 'reselect'
 export const moduleName = 'people'
 const prefix = `${appName}/${moduleName}`
 export const ADD_PERSON = `${prefix}/ADD_PERSON`
+export const ADD_PERSON_SUCCESS = `${prefix}/ADD_PERSON_SUCCESS`
 
 /**
  * Reducer
@@ -51,6 +54,34 @@ export const peopleSelector = createSelector(stateSelector, (state) =>
  * */
 
 export function addPerson(person) {
+  return {
+    type: ADD_PERSON,
+    payload: { person }
+  }
+}
+
+/**
+ * Sagas
+ */
+
+export function* addPersonSaga(action) {
+  const id = yield call(generateId)
+
+  const successAction = {
+    type: ADD_PERSON_SUCCESS,
+    payload: { id, ...action.payload.person }
+  }
+
+  yield put(successAction)
+  yield put(reset('person'))
+}
+
+export function* saga() {
+  yield takeEvery(ADD_PERSON, addPersonSaga)
+}
+
+/*
+export function addPerson(person) {
   return (dispatch) => {
     dispatch({
       type: ADD_PERSON,
@@ -62,3 +93,4 @@ export function addPerson(person) {
     dispatch(reset('person'))
   }
 }
+*/
