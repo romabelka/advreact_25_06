c0110d5769d3006d5952b8f4e5e0a9bb1e2d2308
Max <ruseev.maxim@yandex.ru>
2018-07-07 16:33:24 +0300

    set up firebase

--- admin/src/config.js
+++ admin/src/config.js
@@ -4,12 +4,12 @@ import 'firebase/auth'
 export const appName = 'adv-react-25-06'
 
 const config = {
-  apiKey: 'AIzaSyDzqwnZ_39QyqhxYZVPjVH8eBww7DUBmVc',
-  authDomain: `${appName}.firebaseapp.com`,
-  databaseURL: `https://${appName}.firebaseio.com`,
-  projectId: appName,
+  apiKey: 'AIzaSyD25TAZjq0-smHue8ViLsxv3nWBHm3Pjug',
+  authDomain: 'advreact-bed03.firebaseapp.com',
+  databaseURL: 'https://advreact-bed03.firebaseio.com',
+  projectId: 'advreact-bed03',
   storageBucket: '',
-  messagingSenderId: '874599443389'
+  messagingSenderId: '389238619888'
 }
 
 initializeApp(config)
