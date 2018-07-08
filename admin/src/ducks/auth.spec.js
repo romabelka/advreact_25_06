c949d24e6562184f2c8d2e41657ba1da18d437f6
Max <ruseev.maxim@yandex.ru>
2018-07-08 09:29:41 +0300

    auth spec

--- admin/src/ducks/auth.spec.js
+++ admin/src/ducks/auth.spec.js
@@ -1,6 +1,13 @@
-import { call, put } from 'redux-saga/effects'
+import { call, put, apply } from 'redux-saga/effects'
 import firebase from 'firebase/app'
-import { SIGN_UP_REQUEST, SIGN_UP_SUCCESS, signUpSaga } from './auth'
+import {
+  SIGN_UP_REQUEST,
+  SIGN_UP_SUCCESS,
+  SIGN_IN_REQUEST,
+  SIGN_IN_SUCCESS,
+  signUpSaga,
+  signInSaga
+} from './auth'
 
 describe('Auth duck sign up saga', () => {
   it('should sign up with new email and password', () => {
@@ -33,6 +40,40 @@ describe('Auth duck sign up saga', () => {
     expect(saga.next(user).value).toEqual(
       put({
         type: SIGN_UP_SUCCESS,
+        payload: { user }
+      })
+    )
+  })
+
+  it('should sign in', () => {
+    const auth = firebase.auth()
+
+    const credentials = {
+      email: `test${+new Date()}@test.com`,
+      password: 'qwerty123'
+    }
+
+    const action = {
+      type: SIGN_IN_REQUEST,
+      payload: credentials
+    }
+
+    const saga = signUpSaga(action)
+
+    expect(saga.next().value).toEqual(
+      apply(auth, auth.signInWithEmailAndPassword, [
+        credentials.email,
+        credentials.password
+      ])
+    )
+
+    const user = {
+      email: credentials.email
+    }
+
+    expect(saga.next(user).value).toEqual(
+      put({
+        type: SIGN_IN_SUCCESS,
         payload: { user }
       })
     )
