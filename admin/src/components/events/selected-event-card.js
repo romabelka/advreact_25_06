e66db835e719f0f2c331d84ca60a05b63ff1374a
Roman Iakobchuk <romania@wix.com>
2018-07-09 20:57:39 +0300

    virtualized list

--- admin/src/components/events/selected-event-card.js
+++ admin/src/components/events/selected-event-card.js
@@ -4,7 +4,7 @@ class SelectedEventCard extends Component {
   render() {
     const { event } = this.props
     return (
-      <div>
+      <div style={{ border: '1px solid black', height: 200 }}>
         <h3>{event.title}</h3>
         <h3>{event.where}</h3>
       </div>
--- admin/src/components/events/selected-events.js
+++ admin/src/components/events/selected-events.js
@@ -1,15 +1,27 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import { List } from 'react-virtualized'
 import SelectedEventCard from './selected-event-card'
 import { selectedEventsSelector } from '../../ducks/events'
 
 class SelectedEvents extends Component {
   render() {
+    const { events } = this.props
     return (
-      <div>
-        {this.props.events.map((event) => (
-          <SelectedEventCard event={event} key={event.uid} />
-        ))}
+      <List
+        rowCount={events.length}
+        width={500}
+        height={400}
+        rowHeight={200}
+        rowRenderer={this.rowRenderer}
+      />
+    )
+  }
+
+  rowRenderer = ({ index, key, style }) => {
+    return (
+      <div key={key} style={style}>
+        <SelectedEventCard event={this.props.events[index]} />
       </div>
     )
   }
