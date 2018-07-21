abb9cca8032c47075a9bf53f65c456533b6a760e
Personal <personal@Sergeys-MacBook-Pro.local>
2018-07-21 15:13:35 +0200

    Hotfix

--- admin/src/components/people/person-drag-preview.js
+++ admin/src/components/people/person-drag-preview.js
@@ -4,7 +4,7 @@ import { personSelector } from '../../ducks/people'
 
 class PersonDragPreview extends Component {
   render() {
-    return <div>{this.props.person.firstName}</div>
+    return <div>{this.props.person && this.props.person.firstName}</div>
   }
 }
 
