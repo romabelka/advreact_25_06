66a9930ad4063400ae3e1fbc3ca3a7d2d58a5538
vitaliy <viprogramm@gmail.com>
2018-07-26 00:04:11 +0300

    rewrite EventList from ScrollView to SectionList

--- MobileApp/src/components/event-list.js
+++ MobileApp/src/components/event-list.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import {Text, ScrollView, StyleSheet} from 'react-native'
+import {Text, ScrollView, StyleSheet, SectionList} from 'react-native'
 import Card from './common/card'
 
 class EventList extends Component {
@@ -7,20 +7,28 @@ class EventList extends Component {
 
     };
 
-    render() {
-        return (
-            <ScrollView>
-                {this.props.events.map(event =>
-                    <Card key = {event.uid}>
-                        <Text>{event.title}</Text>
-                    </Card>
-                )}
-            </ScrollView>
-        )
-    }
+  render() {
+    return (
+      <SectionList
+        renderItem={({ item, index, section }) => (
+          <Card key={item.uid}>
+            <Text>{item.title}</Text>
+          </Card>
+        )}
+        renderSectionHeader={({ section: { title } }) => (
+          <Text style={styles.header}>{title}</Text>
+        )}
+        sections={this.props.events}
+        keyExtractor={(item, index) => item + index}
+      />
+    )
+  }
 }
 
 const styles = StyleSheet.create({
+  header: {
+    fontWeight: 'bold'
+  }
 })
 
 export default EventList
