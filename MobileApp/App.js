e41d61125237e9bc4ee0e8b501b0e5d549aacd0e
Personal <personal@Sergeys-MacBook-Pro.local>
2018-07-25 07:38:19 +0200

    HT6.2 rewrite EventList on SectionList

--- MobileApp/App.js
+++ MobileApp/App.js
@@ -10,15 +10,39 @@ const events = Object.entries(data.events).map(([uid, event]) => ({...event, uid
 
 export default class App extends React.Component {
   render() {
+    const groupedEvents = groupEventsByName(events)
+    const sectionListEvents = adjustSectionListData(groupedEvents)
+
     return (
       <View style={styles.container}>
           <Image style = {styles.image}
                  source = {require('./assets/logo.png')}
                  resizeMode = {Image.resizeMode.contain}/>
-          <Event event = {events[0]}/>
+          <EventList events={sectionListEvents}/>
       </View>
     );
   }
+}
+
+function groupEventsByName(events) {
+  return events.reduce((grouped, event) => {
+  	const firstLetter = event.title[0].toUpperCase()
+
+		if (!grouped.get(firstLetter)) {
+	    grouped.set(firstLetter, [])
+    }
+
+		grouped.get(firstLetter).push(event)
+    
+    return grouped
+  }, new Map())
+}
+
+function adjustSectionListData(eventsMap) {
+  return Array.from(eventsMap, ([title, data]) => ({
+  	title,
+    data
+  })).sort((sectionA, sectionB) => sectionA.title > sectionB.title ? 1 : -1)
 }
 
 const styles = StyleSheet.create({
--- MobileApp/src/components/event-list.js
+++ MobileApp/src/components/event-list.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import {Text, ScrollView, StyleSheet} from 'react-native'
+import { View, Text, SectionList, StyleSheet} from 'react-native'
 import Card from './common/card'
 
 class EventList extends Component {
@@ -8,19 +8,47 @@ class EventList extends Component {
     };
 
     render() {
-        return (
-            <ScrollView>
-                {this.props.events.map(event =>
-                    <Card key = {event.uid}>
-                        <Text>{event.title}</Text>
-                    </Card>
-                )}
-            </ScrollView>
-        )
+        return <SectionList
+            renderItem={({ item, index }) => (
+                <Card event={item} key={index}>
+                    <Text>{item.title}</Text>
+                    <Text>{item.when}</Text>
+                    <Text>{item.where}</Text>
+                </Card>
+            )}
+            keyExtractor={(item, index) => item + index}
+            sections={this.props.events}
+            renderSectionHeader={({ section }) => {
+                // debugger
+                return (
+                    <View style={styles.header}>
+                        <Text style={styles.title}>{section.title}</Text>
+                        <Text style={styles.count}>{`Number of Events: ${section.data.length}`}</Text>
+                    </View>
+                )
+            }}
+        />
     }
 }
 
 const styles = StyleSheet.create({
+    header: {
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        backgroundColor: 'white',
+        padding: 10,
+        borderBottomColor: 'black',
+        borderBottomWidth: 2
+    },
+    title: {
+        fontWeight: 'bold',
+        backgroundColor: 'white'
+    },
+    count: {
+        fontWeight: 'bold',
+        backgroundColor: 'white'
+    }
 })
 
 export default EventList
