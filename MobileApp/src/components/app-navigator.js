900f7a537946ab71a4ea7df08e5299037fa81a3d
vitaliy <viprogramm@gmail.com>
2018-07-30 01:22:07 +0300

    add tab navigation between people screen and events screen

--- MobileApp/src/components/app-navigator.js
+++ MobileApp/src/components/app-navigator.js
@@ -1,19 +1,32 @@
-import {createStackNavigator} from 'react-navigation'
+import {createStackNavigator, createBottomTabNavigator, createSwitchNavigator} from 'react-navigation'
 import AuthScreen from './screens/auth'
 import EventListScreen from './screens/event-list'
 import EventScreen from './screens/event'
+import PeopleScreen from './screens/people'
+
+const EventListStack = createStackNavigator({
+  eventList: {
+    screen: EventListScreen,
+    navigationOptions: {
+      title: 'events'
+    }
+  },
+  event: EventScreen,
+});
+
+const PeopleStack = createSwitchNavigator({
+  people: PeopleScreen,
+});
+
+const BottomStack = createBottomTabNavigator(
+  {
+    eventList: EventListStack,
+    people: PeopleStack,
+  }
+);
 
 export default createStackNavigator({
-    auth: {
-        screen: AuthScreen
-    },
-    eventList: {
-        screen: EventListScreen,
-        navigationOptions: {
-            title: 'events'
-        }
-    },
-    event: {
-        screen: EventScreen
-    }
+    auth: AuthScreen,
+    eventList: BottomStack,
+    people: BottomStack
 })
